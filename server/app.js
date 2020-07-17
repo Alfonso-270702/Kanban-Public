2b98caf802152690f72ba25c16c1e05cc0b52dc6
Alfonso-270702 <tebaksiapa27@gmail.com>
2020-07-17 21:18:21 +0700

    add procfile and port change into dinamic

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const route = require('./routes/index')
 const cors = require('cors')
 const errHandler = require('./middleware/errHandler')
