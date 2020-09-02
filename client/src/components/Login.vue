7490e89b722a94a88d652031e0204de9499e101d
Alfonso-270702 <tebaksiapa27@gmail.com>
2020-09-02 12:01:34 +0700

    perbaikin styling

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,74 +1,97 @@
 <template>
-<section>
-  <div class="login-form my-5">
-  <form v-on:submit.prevent="loginForm" >
+  <section>
+    <div class="login-form my-5">
+      <form v-on:submit.prevent="loginForm">
+        <div>
+          <h1 class="title-header text-flex-start">Welcome Back</h1>
+        </div>
+        <div class="form-group my-3">
+          <label for="email">Email:</label>
+          <input
+            type="email"
+            class="form-control w-75"
+            name="email"
+            placeholder="input your email"
+            v-model="email"
+          />
+        </div>
 
-      <div>
-        <h1 class="title-header text-flex-start">Welcome Back</h1>
-      </div>
-      <div class="form-group my-3">
-        <label for="email">Email:</label>
-        <input type="email" class="form-control w-75" name="email" placeholder="input your email" v-model="email">
-      </div>
+        <div class="form-group">
+          <label for="password">Passwrod:</label>
+          <input
+            type="password"
+            class="form-control w-75"
+            name="password"
+            placeholder="input your password"
+            v-model="password"
+          />
+        </div>
 
-      <div class="form-group">
-        <label for="password">Passwrod:</label>
-        <input type="password" class="form-control w-75" name="password" placeholder="input your password" v-model="password">
+        <div class="d-flex justify-content-flex-start">
+          <p v-on:click="registerPage ">
+            Does not have account yet?
+            <span class="button-user">Register here</span>
+          </p>
+        </div>
+        <div class="d-flex">
+          <button class="btn btn-primary">Login</button>
+        </div>
+      </form>
+      <div class="mt-1">
+        <p class="mb-0">Or</p>
       </div>
-
-      <div class="d-flex justify-content-flex-start">
-        <p v-on:click="registerPage ">Does not have account yet? Register here</p>
-       </div>
-      <div class="d-flex">
-        <button class="btn btn-primary">Login</button>
+      <div class="mt-1">
+        <button
+          v-google-signin-button="clientId"
+          class="google-signin-button google-btn btn btn-warning"
+        >
+          <i class="fab fa-google mr-1"></i> Google
+        </button>
       </div>
-    </form>
-    <div class="mt-1">
-      <p class="mb-0">Or</p>
     </div>
-    <div class="mt-1">
-      <button v-google-signin-button="clientId" class="google-signin-button google-btn btn btn-warning"><i class="fab fa-google mr-1"></i> Google</button>
-    </div>
-  </div>
-</section>
+  </section>
 </template>
 
 <script>
 export default {
-  data(){
-    return{
-      email: '',
-      password: '',
-      clientId: '1042655448078-rj8n72r8ck1n8tmjd6a4auebpncbkbrp.apps.googleusercontent.com'
-    }
+  data() {
+    return {
+      email: "",
+      password: "",
+      clientId:
+        "1042655448078-rj8n72r8ck1n8tmjd6a4auebpncbkbrp.apps.googleusercontent.com",
+    };
   },
-  methods:{
-    registerPage(){
-      this.$emit('registerPage')
+  methods: {
+    registerPage() {
+      this.$emit("registerPage");
     },
-    loginForm(){
+    loginForm() {
       let payload = {
         email: this.email,
-        password: this.password
-      }
-      this.$emit('loginForm',payload)
-      this.email = ''
-      this.password = ''
+        password: this.password,
+      };
+      this.$emit("loginForm", payload);
+      this.email = "";
+      this.password = "";
     },
-    OnGoogleAuthSuccess (idToken) {
-      this.$emit('OnGoogleAuthSuccess',idToken)
+    OnGoogleAuthSuccess(idToken) {
+      this.$emit("OnGoogleAuthSuccess", idToken);
       // Receive the idToken and make your magic with the backend
     },
-    OnGoogleAuthFail (error) {
-      console.log(error)
-    }
-  }
-}
+    OnGoogleAuthFail(error) {
+      console.log(error);
+    },
+  },
+};
 </script>
 
 <style>
-  .google-btn{
-    display: flex;
-    align-items: center;
-  }
+.google-btn {
+  display: flex;
+  align-items: center;
+}
+.button-user {
+  cursor: pointer;
+}
 </style>
--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,66 +1,85 @@
 <template>
-<section>
-  <div class="register-form my-5">
-    <form v-on:submit.prevent="registerForm">
-      <div>
-        <h1 title-header text-center>Register</h1>
-      </div>
-      <div class="form-group my-3">
-        <label for="name">Full Name</label>
-        <input type="text" class="form-control w-75" name="name" placeholder="input your full name" v-model="name">
-      </div>
-
-      <div class="form-group">
-        <label for="emai">Email</label>
-        <input type="email" class="form-control w-75" name="email" placeholder="input your email" v-model="email">
-      </div>
-
-      <div class="form-group">
-        <label for="password">Password</label>
-        <input type="password" class="form-control w-75" name="password" placeholder="input your password" v-model="password">
-      </div>
-
-      <div class="d-flex justify-content-flex-start">
-         <p v-on:click="loginPage">Already have account? Login here</p>
+  <section>
+    <div class="register-form my-5">
+      <form v-on:submit.prevent="registerForm">
+        <div>
+          <h1 title-header text-center>Register</h1>
         </div>
-      <div>
-        <button class="btn btn-primary">Register</button>
-      </div>
+        <div class="form-group my-3">
+          <label for="name">Full Name</label>
+          <input
+            type="text"
+            class="form-control w-75"
+            name="name"
+            placeholder="input your full name"
+            v-model="name"
+          />
+        </div>
 
-    </form>
-  </div>
-</section>
+        <div class="form-group">
+          <label for="emai">Email</label>
+          <input
+            type="email"
+            class="form-control w-75"
+            name="email"
+            placeholder="input your email"
+            v-model="email"
+          />
+        </div>
+
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input
+            type="password"
+            class="form-control w-75"
+            name="password"
+            placeholder="input your password"
+            v-model="password"
+          />
+        </div>
+
+        <div class="d-flex justify-content-flex-start">
+          <p v-on:click="loginPage">
+            Already have account?
+            <span class="button-user">Login here</span>
+          </p>
+        </div>
+        <div>
+          <button class="btn btn-primary">Register</button>
+        </div>
+      </form>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-    name: 'Register',
-    data(){
-        return {
-            name: '',
-            email: '',
-            password: '',
-        }
+  name: "Register",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    loginPage() {
+      this.$emit("loginPage");
     },
-    methods:{
-      loginPage(){
-        this.$emit('loginPage')
-      },
-      registerForm(){
-        let payload = {
-          name:this.name,
-          email: this.email,
-          password: this.password
-        }
-        this.$emit('registerForm',payload)
-        this.name = ''
-        this.email = ''
-        this.password = ''
-      }
-    }
-}
+    registerForm() {
+      let payload = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+      };
+      this.$emit("registerForm", payload);
+      this.name = "";
+      this.email = "";
+      this.password = "";
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
